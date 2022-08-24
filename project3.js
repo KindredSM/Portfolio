462184c7a5a88563fddd8ec9360cb16e9c8e333c
Kindred <KDSM@protonmail.com>
2022-08-24 15:29:14 +0100

    fixed broken link and null JS

--- project3.js
+++ project3.js
@@ -1,36 +1,11 @@
-// const options = {
-//   root: null,
-// };
 
-// const navbar = document.querySelector(".navbar");
-// window.onscroll = () => {
-//   if (window.scrollY > 50) {
-//     navbar.classList.add("nav-active");
-//   } else {
-//     navbar.classList.remove("nav-active");
-//   }
-// };
-
-// const observer = new IntersectionObserver(function (entries, observer) {
-//   entries.forEach((entry) => {
-//     if (!entry.isIntersecting) {
-//       return;
-//     }
-//     console.log(entry.target);
-//     entry.target.classList.toggle("fade");
-//     observer.unobserve(entry.target);
-//   });
-// }, options);
-
-// observer.observe(project);
-
-// const menu = document.querySelector("#mobile-menu");
-// const menuLinks = document.querySelector(".navbar__menu");
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar__menu");
 
 // // Display Mobile Menu
-// const mobileMenu = () => {
-//   menu.classList.toggle("is-active");
-//   menuLinks.classList.toggle("active");
-// };
+const mobileMenu = () => {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
+};
 
-// menu.addEventListener("click", mobileMenu);
+menu.addEventListener("click", mobileMenu);
