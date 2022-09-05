2db74cbd86dfa967297905e45fdd2e2cbd6d070c
Kindred <KDSM@protonmail.com>
2022-09-06 00:06:59 +0100

    experiment

--- scripts.js
+++ scripts.js
@@ -1,22 +1,22 @@
-const options = {
-  root: null,
-  treshold: 0,
-};
+// const options = {
+//   root: null,
+//   treshold: 0,
+// };
 
-const observer = new IntersectionObserver(function (entries, observer) {
-  entries.forEach((entry) => {
-    if (!entry.isIntersecting) {
-      return;
-    }
-    console.log(entry.target);
-    entry.target.classList.toggle("fade");
-    observer.unobserve(entry.target);
-  });
-}, options);
+// const observer = new IntersectionObserver(function (entries, observer) {
+//   entries.forEach((entry) => {
+//     if (!entry.isIntersecting) {
+//       return;
+//     }
+//     console.log(entry.target);
+//     entry.target.classList.toggle("fade");
+//     observer.unobserve(entry.target);
+//   });
+// }, options);
 
-observer.observe(about);
-observer.observe(hero__cont);
-observer.observe(projects);
+// observer.observe(about);
+// observer.observe(hero__cont);
+// observer.observe(projects);
 
 const menu = document.querySelector("#mobile-menu");
 const menuLinks = document.querySelector(".navbar__menu");
