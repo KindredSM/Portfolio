a4deac3b567078ab086acf9b17bce3ad3c781425
Kindred <KDSM@protonmail.com>
2022-10-26 17:48:43 +0100

    added GSAP animations

--- scripts.js
+++ scripts.js
@@ -1,46 +1,15 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.from(".github", {
+gsap.from(".hero__container", {
   scrollTrigger: {
-    trigger: ".links__container",
-    start: "20px 80%",
-  },
-  duration: 1,
-  y: -100,
-  opacity: -1,
-  ease: "power4.out",
-});
-gsap.from(".email", {
-  scrollTrigger: {
-    trigger: ".links__container",
-    start: "20px 80%",
-  },
-  duration: 1.3,
-  y: -90,
-  opacity: -1,
-  ease: "power4.out",
-});
-gsap.from(".cv", {
-  scrollTrigger: {
-    trigger: ".links__container",
+    trigger: ".hero__container",
     start: "20px 80%",
   },
   duration: 1.5,
-  y: -80,
+  y: -50,
   opacity: -1,
   ease: "power4.out",
 });
-gsap.from(".linkedin", {
-  scrollTrigger: {
-    trigger: ".links__container",
-    start: "20px 80%",
-  },
-  duration: 2,
-  y: -70,
-  opacity: -1,
-  ease: "power4.out",
-});
-
 gsap.from(".bio__container", {
   scrollTrigger: {
     trigger: ".bio__container",
@@ -58,10 +27,56 @@ gsap.from(".p__header", {
     start: "20px 80%",
   },
   duration: 1.5,
-  x: -50,
+  y: -30,
   opacity: 0,
   ease: "power4.out",
 });
+gsap.from(".projects__wrapper", {
+  scrollTrigger: {
+    trigger: ".projects__wrapper",
+    start: "20px 80%",
+  },
+  duration: 1.5,
+  y: -30,
+  opacity: 0,
+  ease: "bounce",
+});
+gsap.from("#contact", {
+  scrollTrigger: {
+    trigger: "#contact",
+    start: "20px 80%",
+  },
+  duration: 1.5,
+  opacity: 0,
+  ease: "power4.out",
+});
+
+// const scene = new THREE.Scene();
+// const camera = new THREE.PerspectiveCamera(
+//   75,
+//   window.innerWidth / window.innerHeight,
+//   0.1,
+//   1000
+// );
+
+// const renderer = new THREE.WebGL1Renderer();
+
+// renderer.setSize(window.innerWidth, window.innerHeight);
+
+// document.body.appendChild(renderer.domElement);
+
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+// const cube = new THREE.Mesh(geometry, material);
+// scene.add(cube);
+
+// camera.position.z = 5;
+
+// function animate() {
+//   requestAnimationFrame(animate);
+
+//   renderer.render(scene, camera);
+// }
 
 const menu = document.querySelector("#mobile-menu");
 const menuLinks = document.querySelector(".navbar__menu");
