ce8f6768db23dc57f863d74f711dc718e40782cd
Kindred <KDSM@protonmail.com>
2022-10-26 01:26:23 +0100

    added GSAP

--- scripts.js
+++ scripts.js
@@ -1,11 +1,66 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.from(".bio__container", {
-  scrollTrigger: ".bio__container",
+gsap.from(".github", {
+  scrollTrigger: {
+    trigger: ".links__container",
+    start: "20px 80%",
+  },
   duration: 1,
-  x: 50,
+  y: -100,
+  opacity: -1,
+  ease: "power4.out",
+});
+gsap.from(".email", {
+  scrollTrigger: {
+    trigger: ".links__container",
+    start: "20px 80%",
+  },
+  duration: 1.3,
+  y: -90,
+  opacity: -1,
+  ease: "power4.out",
+});
+gsap.from(".cv", {
+  scrollTrigger: {
+    trigger: ".links__container",
+    start: "20px 80%",
+  },
+  duration: 1.5,
+  y: -80,
+  opacity: -1,
+  ease: "power4.out",
+});
+gsap.from(".linkedin", {
+  scrollTrigger: {
+    trigger: ".links__container",
+    start: "20px 80%",
+  },
+  duration: 2,
+  y: -70,
+  opacity: -1,
+  ease: "power4.out",
+});
+
+gsap.from(".bio__container", {
+  scrollTrigger: {
+    trigger: ".bio__container",
+    start: "20px 80%",
+  },
+  duration: 1.5,
+  y: -50,
   opacity: 0,
-  ease: "bounce",
+  ease: "power4.out",
+});
+
+gsap.from(".p__header", {
+  scrollTrigger: {
+    trigger: ".p__header",
+    start: "20px 80%",
+  },
+  duration: 1.5,
+  x: -50,
+  opacity: 0,
+  ease: "power4.out",
 });
 
 const menu = document.querySelector("#mobile-menu");
