18c842eb35c4641ebab72d8e0b1e897eae5e4d66
kindredsm <kdsm@protonmail.com>
2026-05-19 03:32:28 +0100

    mouse

--- cursor.js
+++ cursor.js
@@ -10,21 +10,12 @@ if (canUseCustomCursor) {
 
   let targetX = window.innerWidth / 2;
   let targetY = window.innerHeight / 2;
-  let ringX = targetX;
-  let ringY = targetY;
-
   const moveCursor = (event) => {
     targetX = event.clientX;
     targetY = event.clientY;
     cursorDot.style.transform = `translate3d(${targetX}px, ${targetY}px, 0)`;
+    cursor.style.transform = `translate3d(${targetX}px, ${targetY}px, 0)`;
     document.body.classList.add("cursor-ready");
-  };
-
-  const animateRing = () => {
-    ringX += (targetX - ringX) * 0.18;
-    ringY += (targetY - ringY) * 0.18;
-    cursor.style.transform = `translate3d(${ringX}px, ${ringY}px, 0)`;
-    requestAnimationFrame(animateRing);
   };
 
   const setHoverState = (isHovering) => {
@@ -43,6 +34,4 @@ if (canUseCustomCursor) {
     element.addEventListener("mouseenter", () => setHoverState(true));
     element.addEventListener("mouseleave", () => setHoverState(false));
   });
-
-  requestAnimationFrame(animateRing);
 }
